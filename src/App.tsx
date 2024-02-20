c0aac6f9a2ef0c645dda8d3e89967fc0c79b01e9
Jarvx200 <jarvx404@gmail.com>
2024-02-20 21:32:54 +0200

    bento grid added, buggy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import BentoGrid from "./components/BentoGrid/bentoGrid"
+import BentoGridTile from "./components/BentoGrid/bentoGridTile"
 
 
 function App() {
   return (
     <>
-
+      <BentoGrid lines={3} cols={5} >
+        <BentoGridTile bgHighlight={false} borderHighlight={true} cols={1} lines={2} rounded={true}/>
+        <BentoGridTile bgHighlight={false} borderHighlight={true} cols={1} lines={0} rounded={true}/>
+        <BentoGridTile bgHighlight={false} borderHighlight={true} cols={1} lines={0} rounded={true}/>
+      </BentoGrid>
     </>
   )
 }
